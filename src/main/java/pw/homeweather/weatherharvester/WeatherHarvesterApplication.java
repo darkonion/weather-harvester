099e7ba244c6cc24cc17a542a4a58e46a269cca1
darkonion <pawkapl89@gmail.com>
2020-09-14 19:32:38 +0200

    Initial Commit

---
+++ src/main/java/pw/homeweather/weatherharvester/WeatherHarvesterApplication.java
@@ -0,0 +1,13 @@
+package pw.homeweather.weatherharvester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherHarvesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherHarvesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pw/homeweather/weatherharvester/WeatherHarvesterApplicationTests.java
@@ -0,0 +1,13 @@
+package pw.homeweather.weatherharvester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherHarvesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
